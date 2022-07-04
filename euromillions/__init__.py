b6981befb75d86c65b039dce4c1dc99e96e01b61
Nicolas Voisin <nvoisin@tendancefinance.com>
2022-07-04 16:50:54 +0200

    add imports to init

--- euromillions/__init__.py
+++ euromillions/__init__.py
@@ -1,2 +1,5 @@
 __version__ = "0.1.0"
 __author__ = "Nicolas Voisin"
+
+
+from results import get_results
--- euromillions/main.py
+++ euromillions/main.py
@@ -1,5 +1,5 @@
-from euromillions.results import get_results
+from euromillions import get_results
 
 if __name__ == "__main__":
     res = get_results()
-    print(res.tail(5))
+    print(res.tail(10))
