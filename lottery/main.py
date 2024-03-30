e65ae692735d2d5a4736b3f16ca25cbe157e4775
nick-voisin <voisin.nicolas@gmail.com>
2024-03-30 17:28:58 +0100

    add sorted

--- lottery/main.py
+++ lottery/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from lottery import get_euromillions_results, get_loto_results
-
+import numpy as np
 
 if __name__ == "__main__":
     data_folder = Path(__file__).parents[1] / "data"
@@ -12,3 +12,7 @@ if __name__ == "__main__":
     loto_res.to_csv(data_folder / "loto.csv")
 
     print(loto_res.tail(10))
+
+    sorted_loto = loto_res[["B1", "B2", "B3", "B4", "B5"]].apply(np.sort, axis=1, raw=True)
+    sorted_loto["S1"] = loto_res["S1"]
+    sorted_loto.to_csv(data_folder / "loto_sorted.csv")
